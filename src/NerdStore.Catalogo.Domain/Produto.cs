03740e7732ba7e114c938cac625cedcbd33da070
nicolaujoao1 <ilolanicolau1999@gmail.com>
2023-06-07 00:47:42 +0100

    Adicionando Objecto de valor

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -1,10 +1,11 @@
-﻿using NerdStore.Core.DomainObjects;
+﻿using NerdStore.Catalogo.Domain.ValueObjects;
+using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Produto : Entity, IAggregateRoot
     {
-        public Produto(string? nome, string? descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string? imagem)
+        public Produto(string? nome, string? descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string? imagem, Dimensoes dimensoes)
         {
             CategoriaId = categoriaId;
             Nome = nome;
@@ -13,6 +14,7 @@ namespace NerdStore.Catalogo.Domain
             Valor = valor;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;  
 
             Validar();
         }
@@ -26,6 +28,7 @@ namespace NerdStore.Catalogo.Domain
         public string? Imagem { get;private set; }
         public int QuantidadeEstoque { get; private set; }
         public Categoria? Categoria { get;private set; }
+        public Dimensoes Dimensoes { get; private set; }
 
 
         public void Ativar() => Ativo = true;
---
+++ src/NerdStore.Catalogo.Domain/ValueObjects/Dimensoes.cs
@@ -0,0 +1,28 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain.ValueObjects;
+
+public class Dimensoes
+{
+    public Dimensoes(decimal altura, decimal largura, decimal profundidade)
+    {
+        AssertionConcern.AssertArgumentLessOrEqualThan(Altura, 0, "O campo Altura não pode ser menor ou igual a 0");
+        AssertionConcern.AssertArgumentLessOrEqualThan(Largura, 0, "O campo Largura não pode ser menor ou igual a 0");
+        AssertionConcern.AssertArgumentLessOrEqualThan(Profundidade, 0, "O campo Profundidade não pode ser menor ou igual a 0");
+
+        Altura = altura;
+        Largura = largura;
+        Profundidade = profundidade;
+    }
+
+    public decimal Altura { get; private set; }
+    public decimal Largura { get; private set; }
+    public decimal Profundidade { get; private set; }
+
+    public string DescricaoFormatada() => $"LxAxP: {Largura*Altura*Profundidade}";
+
+    public override string ToString()
+    {
+        return DescricaoFormatada();
+    }
+}
