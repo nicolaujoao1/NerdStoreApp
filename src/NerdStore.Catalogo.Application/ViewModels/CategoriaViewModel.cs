91bb4bf31ed7892a2f4edbeb0d604fbd755d7475
nicolaujoao1 <ilolanicolau1999@gmail.com>
2023-06-16 13:08:41 +0100

    Alteracoes

---
+++ src/NerdStore.Catalogo.Application/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Application.ViewModels
+{
+    public class CategoriaViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int Codigo { get; set; }
+    }
+}
